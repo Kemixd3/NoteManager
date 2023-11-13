5d425a0d58f7b79b82dcbb1d71a20a77548d7aa9
Nikolai <nikolai2002b@gmail.com>
2023-11-13 22:28:40 +0100

    Added functionality

--- src/Account.js
+++ src/Account.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDoc, doc, setDoc } from "firebase/firestore";
-import mammoth from "mammoth";
+
 import { auth, firestore } from "./firebaseClient";
 
 const Account = ({ user }) => {
@@ -59,106 +59,42 @@ const Account = ({ user }) => {
     }
   };
 
-  function DocxParser() {
-    const [parsedData, setParsedData] = useState(null);
-
-    const parseWordDocxFile = (event) => {
-      const file = event.target.files[0];
-      if (!file) return;
-
-      const reader = new FileReader();
-      reader.onloadend = async () => {
-        const arrayBuffer = reader.result;
-        console.log(arrayBuffer);
-        const htmlResult = await mammoth.convertToHtml({
-          arrayBuffer: arrayBuffer,
-        });
-        const rawTextResult = await mammoth.extractRawText({
-          arrayBuffer: arrayBuffer,
-        });
-        const markdownResult = await mammoth.convertToMarkdown({
-          arrayBuffer: arrayBuffer,
-        });
-
-        //const htmlString = htmlResult.value;
-        //const parser = new DOMParser();
-        //const html = parser.parseFromString(htmlString, "text/html");
-
-        // Update the state with the parsed data
-        setParsedData({
-          html: htmlResult.value,
-          rawText: rawTextResult.value,
-          markdown: markdownResult.value,
-          //kage: html,
-        });
-      };
-      reader.readAsArrayBuffer(file);
-    };
-
-    return (
-      <div aria-live="polite">
-        {loading ? (
-          "Saving ..."
-        ) : (
-          <form onSubmit={updateProfile} className="form-widget">
-            <div>Email: {user.email}</div>
-            <div>
-              <label htmlFor="username">Name</label>
-              <input
-                id="username"
-                type="text"
-                placeholder={username}
-                value={username || ""}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="avatar_url">image</label>
-              <input
-                id="avatar_url"
-                type="url"
-                value={avatar_url || ""}
-                onChange={(e) => setAvatarUrl(e.target.value)}
-              />
-            </div>
-            <img id="userImage" src={avatar_url} alt="User Avatar" />
-
-            <div>
-              <button className="button primary block" disabled={loading}>
-                Update profile
-              </button>
-            </div>
-          </form>
-        )}
-
-        <div>
-          <input type="file" onChange={parseWordDocxFile} />
-          {parsedData && (
-            <div>
-              <div>
-                <h3>HTML Content:</h3>
-                {console.log("THIS", parsedData.html)}
-                <div dangerouslySetInnerHTML={{ __html: parsedData.html }} />
-              </div>
-              <div>
-                <h3>Raw Text:</h3>
-                <pre>{parsedData.rawText}</pre>
-              </div>
-              <div>
-                <h3>Markdown Content:</h3>
-                <pre>{parsedData.markdown}</pre>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <DocxParser />
+    <div aria-live="polite">
+      {loading ? (
+        "Saving ..."
+      ) : (
+        <form onSubmit={updateProfile} className="form-widget">
+          <div>Email: {user.email}</div>
+          <div>
+            <label htmlFor="username">Name</label>
+            <input
+              id="username"
+              type="text"
+              placeholder={username}
+              value={username || ""}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="avatar_url">image</label>
+            <input
+              id="avatar_url"
+              type="url"
+              value={avatar_url || ""}
+              onChange={(e) => setAvatarUrl(e.target.value)}
+            />
+          </div>
+          <img id="userImage" src={avatar_url} alt="User Avatar" />
+
+          <div>
+            <button className="button primary block" disabled={loading}>
+              Update profile
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 //import reportWebVitals from "./reportWebVitals";
 
+//<Route path="blogs" element={<Blogs />} />
+//<Route path="contact" element={<Contact />} />
+//<Route path="*" element={<NoPage />} />
+
+export default function Apps() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Apps />);
 
 // to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
