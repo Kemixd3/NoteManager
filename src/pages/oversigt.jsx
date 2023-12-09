2e06185ac9085f3781a2886a3c9d7f6ababf4481
Nikolai <nikolai2002b@gmail.com>
2023-12-09 20:55:38 +0100

    Fix for import name

--- src/pages/oversigt.jsx
+++ src/pages/oversigt.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./POOversigt.css";
+import "./POoversigt.css";
 import Table from "@mui/material/Table"; //Testing out mui table libary
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
